cbc54a090cfd5030f38b4a2f369606ae54d7e6a1
thom974 <thomas.q.luc@gmail.com>
2023-07-12 23:25:06 -0400

    Configure ESLint and Typescript

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,24 +1,16 @@
 /* eslint-env node */
 
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  settings: {
-    react: {
-      version: '18.2'
-    }
-  },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', {
-      allowConstantExport: true
-    }]
-  }
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    rules: {},
+    env: {
+        browser: true,
+        es2021: true,
+    },
 };
