2369255d594a610021f4641f3fa59d3ec10e74d1
Kevin Liu <kevinliu676@yahoo.ca>
2023-07-15 17:22:24 -0400

    Sending images to DB works

--- backend/models/image.model.ts
+++ backend/models/image.model.ts
@@ -1,24 +1,19 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-export interface IImage extends Document {
-  name: string;
-  image: {
-    data: Buffer;
-    contentType: string;
-  };
-}
+interface IImage extends Document {
+    image: {
+      data: Buffer;
+      contentType: string;
+    };
+  } 
 
-const imageSchema: Schema<IImage> = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  image: {
-    data: Buffer,
-    contentType: String,
-  },
-});
+const imageSchema : Schema = new Schema({
+    image: {
+        data: Buffer,
+        contentType: String
+    }
+})
 
-const Image = mongoose.model<IImage>('Image', imageSchema);
+const ImageModel = mongoose.model<IImage>('Image',imageSchema);
 
-export default Image;
+export default ImageModel;
--- backend/server.ts
+++ backend/server.ts
@@ -2,13 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import mongoose, { ConnectOptions } from 'mongoose';
 import dotenv from 'dotenv';
-import path from 'path'
-import fs from 'fs'
 import multer from 'multer'
 import ImageModel from './models/image.model';
-import { IImage } from './models/image.model';
-import DataModel from './models/data.model';
-
 
 dotenv.config();
 
@@ -38,85 +33,34 @@ connection.once('open', () => {
     console.log("MongoDB database connection established succesfully");
 })
 
-app.post('/', (req, res) => {
-    const { message } = req.body;
-  
-    const newData = new DataModel({
-      message,
-    });
-  
-    newData
-      .save()
-      .then(() => res.send('Sent successfully'))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).send('Failed to send');
-      });
-  });
-
-/*
-
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
-});
-
-const Storage = multer.diskStorage({
-    destination: "uploads",
+const storage = multer.diskStorage({
+    destination: 'uploads',
     filename: (req, file, cb) => {
         cb(null, file.originalname);
     }
 });
 
-// var upload = multer({ storage: storage });
-
 const upload = multer({
     storage: storage
-}).single('testImage')
+}).single('image')
 
-app.post('/', (req, res) => {
+app.post('/upload', (req, res) => {
     upload(req, res, (err) => {
         if (err) {
             console.log(err);
         } 
         else {
-            const newImage: IImage = new ImageModel({
-                name: req.body.name,
+            const newImage = new ImageModel({
                 image: {
-                    data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file!.filename)),
+                    data: req.file?.filename,
                     contentType: 'image/png'
                 },
             });
 
             newImage
                 .save()
-                .then(() => res.send('successfully uploaded'))
+                .then(() => res.send('Successfully uploaded'))
                 .catch((err) => console.log(err));
         }
     });
 });
-
-/*
-app.post('/', upload.single('image'), (req, res, next) => {
-    var obj = {
-        name: req.body.name,
-        image: {
-            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file!.filename)),
-            contentType: 'image/png'
-        }
-    }
-    imageSchema.create(obj)
-        .then((item) => {
-            item.save()
-                .then(() => res.send("successfully uploaded"));
-            res.redirect('/');
-        })
-        .catch((err) => {
-            console.log(err);
-    });
-});
-*/
