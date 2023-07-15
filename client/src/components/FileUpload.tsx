0b42fe3ed4ba58d5353908f34e794413e1591619
thom974 <thomas.q.luc@gmail.com>
2023-07-15 17:15:15 -0400

    Create FileUpload component

---
+++ client/src/components/FileUpload.tsx
@@ -0,0 +1,7 @@
+export default function FileUpload() {
+    return (
+        <div className='flex items-center justify-center border-dashed border-gray-200 border-2'>
+            <div>Upload Files</div>
+        </div>
+    );
+}
