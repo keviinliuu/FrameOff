e1320f990ab47c51b0205768bbb91116fb5cd616
DongtaiS <dongtaishi.w@gmail.com>
2023-07-15 15:58:27 -0400

    route

--- backend/server.ts
+++ backend/server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import mongoose, { ConnectOptions } from 'mongoose';
 import dotenv from 'dotenv';
-import multer from 'multer';
-
+import path from 'path'
+import fs from 'fs'
+import multer from 'multer'
 dotenv.config();
 
 const app = express();
@@ -14,8 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(uri, {
-    useNewUrlParser: true,
+
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
     wtimeoutMS: 2500,
     useUnifiedTopology: true
 } as ConnectOptions).catch(err => {
@@ -30,4 +32,41 @@ mongoose.connect(uri, {
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established succesfully");
+})
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage });
+
+app.post('/', upload.single('image'), (req, res, next) => {
+ 
+    var obj = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+    imgSchema.create(obj)
+    .then ((err, item) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // item.save();
+            res.redirect('/');
+        }
+    });
+});
+
+app.post('/', (req, res) => {
+    connection.db
 })
