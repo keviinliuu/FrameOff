aa1512e0b1ea61e1fdd3fccee13b942af6c76c1c
Kevin Liu <kevinliu676@yahoo.ca>
2023-07-25 19:17:38 -0400

    Implement rate limiting

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import cors from 'cors';
 import mongoose, { ConnectOptions } from 'mongoose';
 import dotenv from 'dotenv';
@@ -12,10 +13,17 @@ const app = express();
 const port = process.env.PORT;
 const uri: string = process.env.FRAMEOFF_DB_URI!;
 
+// Configure rate limit middleware
+const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 1000, // Maximum 1000 requests per hour per IP address
+});
+
 // Configure Express server
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
+app.use(limiter);
 app.use(imagesRouter);
 app.use(duelsRouter);
 
@@ -29,12 +37,12 @@ mongoose.connect(uri, {
     process.exit(1);
 }).then(() => {
     app.listen(port, () => {
-        console.log(`Server is listening port ${port}`);
+        console.log(`Server is listening on port ${port}`);
     });
 });
 
 // Log successful MongoDB connection
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established succesfully");
+    console.log('MongoDB database connection established succesfully');
 });
