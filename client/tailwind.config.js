eb2ed294eefe928b35c756ebedc9e511d236c79a
Kevin Liu <kevinliu676@yahoo.ca>
2023-08-06 02:05:09 -0400

    Add colours

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,22 @@
 export default {
     content: ['./index.html', './src/index.tsx', './**/*.{js,ts,jsx,tsx}'],
     theme: {
+        colors: {
+            'current': 'currentColor',
+            'moonbeam': '#ffffff',
+            'slate': '#838383',
+            'graphite': '#585457',
+            'charcoal': '554f4f',
+            'aqua': '#84e4db',
+            'midnight': '#00031d',
+            'nocturne': '#0d0324',
+            'blush': '#ffade7',
+            'lilac': '#bc8aad',
+            'candy': '#ffd4f3',
+            'plum': '#6b385c',
+            'raspberry': '#be4f9d',
+            'fushcia': '#e41b9f'
+        },
         extend: {},
     },
     plugins: [],
