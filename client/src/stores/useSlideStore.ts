cd3d0294205c78ca7ab37602e8ecc8d1b052797f
Thomas <thomas.q.luc@gmail.com>
2023-08-08 16:47:55 -0400

    Add to store actions

--- client/src/stores/useSlideStore.ts
+++ client/src/stores/useSlideStore.ts
@@ -1,26 +1,31 @@
 import { create } from 'zustand';
-
 import { SlideData } from '../data/types';
+import axios from 'axios';
 
 export type State = {
     slides: SlideData[];
 };
 
 export type Action = {
-    addSlide(slide: SlideData): void;
-    editSlide(id: string, slide: SlideData): void;
-    loadSlides(): void;
+    addSlide: (slide: SlideData) => void;
+    editSlide: (id: string, slide: SlideData) => void;
+    loadSlides: (id: string) => void;
 };
 
-export const useSlideStore = create<State & Action>()(set => ({
+export const useSlideStore = create<State & Action>()((set, get) => ({
     slides: [],
-    addSlide(slide: SlideData) {
-        console.log(`A slide with title ${slide.title} was added!`);
+    addSlide: (slide: SlideData) => {
+        set(state => ({
+            slides: [...state.slides, slide],
+        }));
     },
-    editSlide(id: string, slide: SlideData) {
-        console.log(`A slide with id ${id} and title ${slide.title} was edited!`);
+    editSlide: (id: string, slide: SlideData) => {
+        console.log(id);
+        console.log(slide);
     },
-    loadSlides() {
-        console.log('A whole bunch of slides was just loaded.');
+    loadSlides: (id: string) => {
+        axios
+            .get(`/${id}`)
+            .then(res => res.data.slides.forEach((slide: SlideData) => get().addSlide(slide)));
     },
 }));
