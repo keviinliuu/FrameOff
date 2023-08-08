ff7dbdd3f744d9762e8236a52ef97540184abd0d
Thomas <thomas.q.luc@gmail.com>
2023-08-08 16:46:41 -0400

    Move work out of CreatePoll, update ViewPoll page

--- client/src/pages/CreatePoll.tsx
+++ client/src/pages/CreatePoll.tsx
@@ -1,55 +1,10 @@
-import FileUpload from '../components/FileUpload';
-
-import { ChangeEvent } from 'react';
-
 import { useSlideStore } from '../stores/useSlideStore';
 
-export interface SlideProps {
-    handleTitle(e: ChangeEvent<HTMLInputElement>): void;
-    handleDescription(e: ChangeEvent<HTMLInputElement>): void;
-    imageOne: File;
-    handleImageOne(): void;
-    imageTwo: File;
-    handleImageTwo(): void;
-}
-
-function Slide({
-    handleTitle,
-    handleDescription,
-    imageOne,
-    handleImageOne,
-    imageTwo,
-    handleImageTwo,
-}: SlideProps) {
-    return (
-        <div className='flex flex-col gap-y-4'>
-            <input onChange={handleTitle} placeholder='Enter a title...' />
-            <input onChange={handleDescription} placeholder='Enter a description...' />
-            <div className='flex gap-x-4'>
-                <FileUpload image={imageOne} onChange={handleImageOne} />
-                <FileUpload image={imageTwo} onChange={handleImageTwo} />
-            </div>
-        </div>
-    );
-}
-
 export default function CreatePoll() {
+    const slides = useSlideStore(state => state.slides);
     const handleSubmit = () => {
         console.log('submitted, for now.');
     };
 
-    useSlideStore(state => state.loadSlides());
-
-    return (
-        <div className='flex flex-col gap-y-4 items-center'>
-            <form
-                className='flex flex-col gap-y-8 h-full items-center'
-                onSubmit={handleSubmit}
-                encType='multipart/form-data'>
-                <div className='flex gap-x-8 h-full'></div>
-                <button>Create Poll</button>
-            </form>
-            <button>Add Slide</button>
-        </div>
-    );
+    return <div></div>;
 }
--- client/src/pages/ViewPoll.tsx
+++ client/src/pages/ViewPoll.tsx
@@ -1,3 +1,29 @@
-export default function ViewPoll() {
-    return <div>This is the view poll page!</div>;
+import { useSlideStore } from '../stores/useSlideStore';
+import { useEffect } from 'react';
+import SlideView from '../components/SlideView';
+
+export interface ViewPollProps {
+    _id: string;
+}
+
+export default function ViewPoll({ _id }: ViewPollProps) {
+    const loadSlides = useSlideStore(state => state.loadSlides);
+    const slides = useSlideStore(state => state.slides);
+    useEffect(() => {
+        loadSlides(_id);
+        // eslint-disable-next-line react-hooks/exhaustive-deps -- only execute once on mount
+    }, []);
+    return (
+        <div className='flex flex-col gap-y-6'>
+            {slides.map(slide => (
+                <SlideView
+                    key={slide.id}
+                    title={slide.slideTitle}
+                    description={slide.slideDescription}
+                    imageOne={slide.image1}
+                    imageTwo={slide.image2}
+                />
+            ))}
+        </div>
+    );
 }
