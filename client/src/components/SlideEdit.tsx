b343c26c9b8f4ddf0a73846e0e3273475802746f
Thomas <thomas.q.luc@gmail.com>
2023-08-09 16:58:36 -0400

    Make image props uncontrolled, use Zustand subscribe

--- client/src/components/SlideEdit.tsx
+++ client/src/components/SlideEdit.tsx
@@ -1,43 +1,78 @@
-import { ChangeEvent } from 'react';
 import Slide from './Slide';
 import FileUpload from './FileUpload';
-
-type CE = ChangeEvent<HTMLInputElement>;
+import { CE } from '../data/types';
+import { useEffect, useState } from 'react';
+import { useSlideStore } from '../stores/useSlideStore';
 
 export interface SlideEditProps {
+    _id?: string;
     handleTitle: (e: CE) => void;
     handleDescription: (e: CE) => void;
-    imageOne: File | undefined;
     handleImageOne: (image: File) => void;
     handleImageOneCaption: (e: CE) => void;
-    imageTwo: File | undefined;
     handleImageTwo: (image: File) => void;
     handleImageTwoCaption: (e: CE) => void;
 }
 
 export default function SlideEdit({
+    _id,
     handleTitle,
     handleDescription,
-    imageOne,
     handleImageOne,
     handleImageOneCaption,
-    imageTwo,
     handleImageTwo,
     handleImageTwoCaption,
 }: SlideEditProps) {
+    const [imageOne, setImageOne] = useState<File>();
+    const [imageTwo, setImageTwo] = useState<File>();
+
+    useEffect(
+        () =>
+            useSlideStore.subscribe(
+                state => state.slides.find(slide => slide._id === _id),
+                newState => {
+                    const newURLOne = newState!.image1.url as File;
+                    const newURLTwo = newState!.image2.url as File;
+                    if (imageOne !== newURLOne) setImageOne(newURLOne);
+                    if (imageTwo !== newURLTwo) setImageTwo(newURLTwo);
+                },
+            ),
+        // eslint-disable-next-line react-hooks/exhaustive-deps -- Need to only subscribe one upon mount.
+        [],
+    );
     return (
         <Slide>
-            <input onChange={handleTitle} placeholder='Enter a title...' />
-            <input onChange={handleDescription} placeholder='Enter a description...' />
-            <div className='flex gap-x-8'>
-                <div className='aspect-square w-96'>
-                    <FileUpload image={imageOne} onChange={handleImageOne} />
-                    <input onChange={handleImageOneCaption} />
+            <input
+                className='w-1/3 placeholder:text-center text-center'
+                onChange={handleTitle}
+                placeholder='Enter a title...'
+            />
+            <input
+                className='text-sm w-1/3 placeholder:text-center text-center'
+                onChange={handleDescription}
+                placeholder='Enter a description...'
+            />
+            <div className='flex gap-x-8 justify-center'>
+                <div className='flex flex-col gap-y-4 items-center'>
+                    <div className='aspect-square w-96'>
+                        <FileUpload image={imageOne} onChange={handleImageOne} />
+                    </div>
+                    <input
+                        className='placeholder:text-center text-center text-sm'
+                        onChange={handleImageOneCaption}
+                        placeholder='Enter a caption...'
+                    />
                 </div>
-                <div className='text-xs'>VS</div>
-                <div className='aspect-square w-96'>
-                    <FileUpload image={imageTwo} onChange={handleImageTwo} />
-                    <input onChange={handleImageTwoCaption} />
+                <div className='flex text-xs items-center'>VS</div>
+                <div className='flex flex-col gap-y-4 items-center'>
+                    <div className='aspect-square w-96'>
+                        <FileUpload image={imageTwo} onChange={handleImageTwo} />
+                    </div>
+                    <input
+                        className='placeholder:text-center text-center text-sm'
+                        onChange={handleImageTwoCaption}
+                        placeholder='Enter a caption...'
+                    />
                 </div>
             </div>
         </Slide>
