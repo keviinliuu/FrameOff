0d0f619f6c048fa8cafce6f8359ed1059c2fff6b
Thomas <thomas.q.luc@gmail.com>
2023-08-09 16:58:46 -0400

    Change styling

--- client/src/components/SlideView.tsx
+++ client/src/components/SlideView.tsx
@@ -13,13 +13,13 @@ export default function SlideView({ title, description, imageOne, imageTwo }: Sl
         <Slide>
             {title && <div className='text-lg'>{title}</div>}
             {description && <div className='text-sm'>{description}</div>}
-            <div className='flex gap-x-8'>
-                <div className='flex flex-col gap-y-4'>
-                    <img src={imageOne.url} className='aspect-square w-96' />
+            <div className='flex justify-between'>
+                <div className='flex flex-col gap-y-4 items-center'>
+                    <img src={imageOne.url as string} className='aspect-square w-96' />
                     {imageOne.caption && <div className='text-sm'>{imageOne.caption}</div>}
                 </div>
-                <div className='flex flex-col gap-y-4'>
-                    <img src={imageTwo.url} className='aspect-square w-96' />
+                <div className='flex flex-col gap-y-4 items-center'>
+                    <img src={imageTwo.url as string} className='aspect-square w-96' />
                     {imageTwo.caption && <div className='text-sm'>{imageTwo.caption}</div>}
                 </div>
             </div>
