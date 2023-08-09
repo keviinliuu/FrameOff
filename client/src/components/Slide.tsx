9c36d03d1ef394f7fe2e17785fc56dab58072539
Thomas <thomas.q.luc@gmail.com>
2023-08-09 16:56:36 -0400

    Change base Slide

--- client/src/components/Slide.tsx
+++ client/src/components/Slide.tsx
@@ -5,5 +5,9 @@ export interface SlideProps {
 }
 
 export default function Slide({ children }: SlideProps) {
-    return <div className='border-neutral-25 flex flex-col gap-y-6 rounded border'>{children}</div>;
+    return (
+        <div className='p-6 border-neutral-25 flex flex-col gap-y-6 rounded border items-center'>
+            {children}
+        </div>
+    );
 }
