00ebc30cc71176d91549c5486c459a8f06ba8aa8
Thomas <thomas.q.luc@gmail.com>
2023-08-09 17:00:47 -0400

    Add utility types and update Slide/Image types

--- client/src/data/types.ts
+++ client/src/data/types.ts
@@ -1,16 +1,22 @@
+import { ChangeEvent } from 'react';
+
+export type CE = ChangeEvent<HTMLInputElement>;
+
+export const MIMES = ['image/png', 'image/jpeg'];
+
 export interface ImageData {
     _id?: string;
-    url: string;
+    url: string | File | null;
     caption?: string;
     votes?: number;
 }
 
 export interface SlideData {
-    id?: string;
+    _id?: string;
     slideTitle?: string;
     slideDescription?: string;
     index?: number;
     image1: ImageData;
     image2: ImageData;
-    __v: number;
+    __v?: number;
 }
