eafec2b29625968ed972eed8c696cd16dfe3a3f1
Kevin Liu <kevinliu676@yahoo.ca>
2023-08-09 23:03:39 -0400

    Voting for images WIP

--- client/src/components/SlideView.tsx
+++ client/src/components/SlideView.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ImageData, VotedEnum } from '../data/types';
 import Slide from './Slide';
 import VoteImage from './VoteImage';
@@ -8,30 +9,68 @@ export interface SlideViewProps {
     description: string | undefined;
     imageOne: ImageData;
     imageTwo: ImageData;
-    _id : string | undefined;
+    _id: string | undefined;
 }
 
-
 export default function SlideView({ title, description, imageOne, imageTwo, _id }: SlideViewProps) {
-    function onVote(imageEnum : VotedEnum) {
+    const [votedFor, setVote] = useState('');
+
+    function onVote(imageEnum: VotedEnum) {
+        setVote(imageEnum);
+
         axios
             .patch(`/voteslide`, {
-                _id : _id,
-                votedFor : imageEnum
+                _id: _id,
+                votedFor: imageEnum,
             })
             .then(res => console.log(res.data));
     }
+
     return (
         <Slide>
             {title && <div className='text-lg'>{title}</div>}
             {description && <div className='text-sm'>{description}</div>}
             <div className='flex justify-between'>
-                <div className='flex flex-col gap-y-4 items-center'>
-                    <VoteImage imgUrl={imageOne.url as string} voteEnum={ VotedEnum.IMAGE1 } onVote = {onVote} />
+                <div className='relative flex flex-col items-center gap-y-4'>
+                    <VoteImage
+                        imgUrl={imageOne.url as string}
+                        voteEnum={VotedEnum.IMAGE1}
+                        onVote={onVote}
+                    />
+
+                    {votedFor === 'IMAGE1' && (
+                        <div className='absolute inset-0 flex h-96 w-96 items-center justify-center bg-fuchsia-500 bg-opacity-50'>
+                            <p className='text-xl font-bold text-white'>Voted for 1!</p>
+                        </div>
+                    )}
+
+                    {votedFor !== 'IMAGE1' && votedFor !== '' && (
+                        <div className='absolute inset-0 flex h-96 w-96 items-center justify-center bg-slate bg-opacity-50'>
+                            <p className='text-xl font-bold text-white'>Not voted for 1</p>
+                        </div>
+                    )}
+
                     {imageOne.caption && <div className='text-sm'>{imageOne.caption}</div>}
                 </div>
-                <div className='flex flex-col gap-y-4 items-center'>
-                    <VoteImage imgUrl={imageTwo.url as string} voteEnum={ VotedEnum.IMAGE2 } onVote = {onVote} />
+                <div className='relative flex flex-col items-center gap-y-4'>
+                    <VoteImage
+                        imgUrl={imageTwo.url as string}
+                        voteEnum={VotedEnum.IMAGE2}
+                        onVote={onVote}
+                    />
+
+                    {votedFor === 'IMAGE2' && (
+                        <div className='absolute inset-0 flex h-96 w-96 items-center justify-center bg-fuchsia-500 bg-opacity-50'>
+                            <p className='text-xl font-bold text-white'>Voted for 2!</p>
+                        </div>
+                    )}
+
+                    {votedFor !== 'IMAGE2' && votedFor !== '' && (
+                        <div className='absolute inset-0 flex h-96 w-96 items-center justify-center bg-slate bg-opacity-50'>
+                            <p className='text-xl font-bold text-white'>Not voted for 2</p>
+                        </div>
+                    )}
+
                     {imageTwo.caption && <div className='text-sm'>{imageTwo.caption}</div>}
                 </div>
             </div>
--- client/src/components/VoteImage.tsx
+++ client/src/components/VoteImage.tsx
@@ -1,15 +1,22 @@
-import { VotedEnum } from "../data/types";
+import { useState } from 'react';
+import { VotedEnum } from '../data/types';
 
 export interface VoteImageProps {
     imgUrl: string | undefined;
-    voteEnum : VotedEnum;
+    voteEnum: VotedEnum;
     onVote(choice: VotedEnum): void;
 }
 
 export default function VoteImage({ imgUrl, voteEnum, onVote }: VoteImageProps) {
     return (
-        <div className='flex flex-col gap-y-4 items-center'>
-            <img onClick = {e => onVote(voteEnum)} src={imgUrl as VotedEnum} className='aspect-square w-96 object-cover' />
+        <div className='flex flex-col items-center gap-y-4'>
+            <img
+                onClick={() => {
+                    onVote(voteEnum);
+                }}
+                src={imgUrl as VotedEnum}
+                className='aspect-square w-96 object-cover'
+            />
         </div>
     );
 }
