457b9edfa6c956648dd76f1b0338508dc09c1273
Kevin Liu <kevinliu676@yahoo.ca>
2023-08-14 21:48:05 -0400

    Add percentage bars

--- client/src/components/SlideView.tsx
+++ client/src/components/SlideView.tsx
@@ -42,8 +42,8 @@ export default function SlideView({ title, description, imageOne, imageTwo, _id 
             setVotesImage2(votesImage2);
             setVotesPercent2(votesPercent2);
 
-            setImage1BarSize(`h-[${votesPercent1}%]`);
-            setImage2BarSize(`h-[${votesPercent2}%]`);
+            setImage1BarSize(`${votesPercent1}%`);
+            setImage2BarSize(`${votesPercent2}%`);
 
             didVote(true);
         } catch (error) {
@@ -73,7 +73,9 @@ export default function SlideView({ title, description, imageOne, imageTwo, _id 
                                             </p>
                                         </div>
 
-                                        <div className={`w-6 bg-fushcia ${image1BarSize}`}></div>
+                                        <div
+                                            className={`w-6 bg-fushcia`}
+                                            style={{ height: image1BarSize }}></div>
                                     </div>
                                 ) : (
                                     <div className='flex h-96 items-end'>
@@ -83,7 +85,9 @@ export default function SlideView({ title, description, imageOne, imageTwo, _id 
                                             </p>
                                         </div>
 
-                                        <div className={`w-6 bg-slate ${image1BarSize}`}></div>
+                                        <div
+                                            className={`h-1/2 w-6 bg-slate`}
+                                            style={{ height: image1BarSize }}></div>
                                     </div>
                                 )}
                             </div>
@@ -93,41 +97,42 @@ export default function SlideView({ title, description, imageOne, imageTwo, _id 
                     {imageOne.caption && <div className='text-sm'>{imageOne.caption}</div>}
                 </div>
                 <div className='relative flex flex-col items-center gap-y-4'>
-                    <div className='relative flex flex-row items-end space-x-2'>
+                    <div className='relative flex flex-row items-end space-x-2'>           
+                        {voted && (
+                            <div>
+                                {votedFor === 'IMAGE2' ? (
+                                    <div className='flex h-96 items-end'>
+                                        <div
+                                            className={`w-6 bg-fushcia`}
+                                            style={{ height: image2BarSize }}></div>
+                                            
+                                        <div className='absolute top-0 right-0 flex h-96 w-96 items-center justify-center border-4 border-fushcia bg-fuchsia-500 bg-opacity-50'>
+                                            <p className='text-main text-xl text-moonbeam'>
+                                                {votesImage2} votes, {votesPercent2}%
+                                            </p>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div className='flex h-96 items-end'>
+                                        <div
+                                            className={`w-6 bg-slate`}
+                                            style={{ height: image2BarSize }}></div>
+
+                                        <div className='absolute top-0 right-0 flex h-96 w-96 items-center justify-center bg-slate bg-opacity-50'>
+                                            <p className='text-main text-xl text-moonbeam'>
+                                                {votesImage2} votes, {votesPercent2}%
+                                            </p>
+                                        </div>
+                                    </div>
+                                )}
+
+                            </div>
+                        )}
                         <VoteImage
                             imgUrl={imageTwo.url as string}
                             voteEnum={VotedEnum.IMAGE2}
                             onVote={onVote}
                         />
-                        {voted && (
-                            <div>
-                                {votedFor === 'IMAGE2' ? (
-                                    <div className='flex h-96 items-end'>
-        
-
-                                        <div className='absolute inset-0 flex h-96 w-96 items-center justify-center border-4 border-fushcia bg-fuchsia-500 bg-opacity-50'>
-                                            <p className='text-main text-xl text-moonbeam'>
-                                                {votesImage2} votes, {votesPercent2}%
-                                            </p>
-                                        </div>
-
-                                        <div className={`w-6 bg-fushcia ${image2BarSize}`}></div>
-                                    </div>
-                                ) : (
-                                    <div className='flex h-96 items-end'>
-                                        
-
-                                        <div className='absolute inset-0 flex h-96 w-96 items-center justify-center bg-slate bg-opacity-50'>
-                                            <p className='text-main text-xl text-moonbeam'>
-                                                {votesImage2} votes, {votesPercent2}%
-                                            </p>
-                                        </div>
-
-                                        <div className={`w-6 bg-slate ${image2BarSize}`}></div>
-                                    </div>
-                                )}
-                            </div>
-                        )}
                     </div>
 
                     {imageTwo.caption && <div className='text-sm'>{imageTwo.caption}</div>}
