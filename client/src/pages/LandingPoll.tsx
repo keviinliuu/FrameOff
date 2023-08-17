ace60585ce2d0c3c30afc701e84b0b4d2c66c83e
Raymond Tao <taor10@mcmaster.ca>
2023-08-16 23:34:57 -0400

    fix

---
+++ client/src/pages/LandingPoll.tsx
@@ -0,0 +1,40 @@
+import ShareButton from '../components/ShareButton';
+import Heading from '../components/Heading';
+import Logo from '../components/Logo';
+import '../index.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
+export default function LandingPoll() {
+    return (
+        <div className='h-full bg-midnight'>
+            <div className='flex justify-center'>
+                <Logo />
+            </div>
+
+            <div className='flex flex-col items-center pt-24'>
+                <Heading textColor='text-blush' fontSize='text-5xl' textAlign='text-center'>
+                    Slide, Vote, Repeat.
+                </Heading>
+            </div>
+            <div className='flex flex-col items-center pt-8'>
+                <Heading textColor='text-white' fontSize='text-1.5xl' textAlign='text-center'>
+                Create and share image polls in seconds.
+                </Heading>
+            </div>
+            <div className='flex justify-center pt-12'>
+                <button className='flex items-center rounded border-2 border-blush'>
+                    <Heading
+                        textColor='text-blush'
+                        fontSize='text-2xl'
+                        padding='py-1/2 px-6'
+                        textAlign='text-center'>
+                        Create Poll
+                    </Heading>
+                </button>
+            </div>
+        </div>
+    );
+}
---
+++ client/src/stories/StartPoll.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import StartPoll from '../pages/StartPoll';
+
+const meta: Meta<typeof StartPoll> = {
+    component: StartPoll,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof StartPoll>;
+
+export const Main: Story = {
+    render: () => (
+        <div className='flex h-screen'>
+            <StartPoll />
+        </div>
+    ),
+};
