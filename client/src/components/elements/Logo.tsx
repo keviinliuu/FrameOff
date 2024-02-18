1c714960cc8411128db9da68adc75d5af95e46f8
Kevin Liu <kevinliu676@yahoo.ca>
2024-02-18 14:48:52 -0500

    Fine tune share page

--- client/src/components/elements/Logo.tsx
+++ client/src/components/elements/Logo.tsx
@@ -3,7 +3,7 @@ import FrameOffLogo from '../../assets/frameoff-logo.svg';
 export default function Logo({}) {
     return (
         <div className='p-8 inline-flex position: absolute top-0 left-0'>
-            <a className='z-10' href='https://www.frameoff.com'>
+            <a href='https://www.frameoff.com'>
                 <img className='h-10 aspect-{25/6}' src={FrameOffLogo} />
             </a>
         </div>
--- client/src/pages/SharePoll.tsx
+++ client/src/pages/SharePoll.tsx
@@ -1,65 +1,53 @@
-import { useSlideStore } from '../stores/useSlideStore'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { useSlideStore } from '../stores/useSlideStore';
+import { SlCopyButton } from '@shoelace-style/shoelace/dist/react';
 
 import ShareButton from '../components/elements/ShareButton';
 import Heading from '../components/elements/Heading';
 import Logo from '../components/elements/Logo';
+import '../index.css';
 
 export default function SharePoll() {
-    const pollId = useSlideStore(state => state.pollId)
-    const pollTitle = 'My Awesome Poll';
+    const pollId = useSlideStore(state => state.pollId);
+    const pollTitle = useSlideStore(state => state.pollTitle);
     const pollUrl = `https://frameoff.me/${pollId}`;
 
-    const [_, setIsCopied] = useState(false);
-
-    const handleCopy = () => {
-        setIsCopied(true);
-    };
-
     return (
-        <div className='flex flex-col justify-center'>
+        <div className='flex flex-col justify-center items-center min-h-screen w-screen'>
             <div className='flex'>
                 <Logo />
             </div>
 
-            <div className='flex flex-col items-center pt-20'>
+            <div className='flex flex-col items-center pb-24 px-4 pt-4'>
                 <Heading textColor='text-moonbeam' fontSize='text-3xl' textAlign='text-center'>
                     Thanks for using FrameOff! Now go share your poll with the world 🌎
                 </Heading>
             </div>
 
-            <div className='flex flex-col items-center pt-20'>
-                <div className='w-2/5'>
+            <div className='flex flex-col pb-10 w-[28rem]'>
+                <div className=''>
                     <Heading textColor='text-blush' fontSize='text-3xl'>
                         Share via link
                     </Heading>
                 </div>
 
-                <div className='m-1 flex h-12 w-2/5 flex-row rounded border-2 border-blush'>
+                <div className='flex h-12 flex-row rounded border-2 border-blush'>
                     <Heading textColor='text-candy' fontSize='text-2xl' padding='pt-2 pl-4'>
                         {pollUrl}
                     </Heading>
 
-                    <div className='flex grow'>
-                        <CopyToClipboard text={pollUrl} onCopy={handleCopy}>
-                            <button className='ml-56'>
-                                <FontAwesomeIcon
-                                    icon={faCopy}
-                                    beat
-                                    size='lg'
-                                    style={{ color: '#FFADE7' }}
-                                />
-                            </button>
-                        </CopyToClipboard>
+                    <div className='ml-auto  text-blush pt-0.5 text-2xl'>
+                        <SlCopyButton
+                            value={pollUrl}
+                            copy-label='Click to copy'
+                            success-label='Copied!'
+                            error-label="Whoops, your browser doesn't support this!"
+                        />
                     </div>
                 </div>
             </div>
 
-            <div className='flex flex-col items-center space-y-1.5 pt-20'>
-                <div className='w-2/5'>
+            <div className='flex flex-col space-y-1.5 pt-20 w-[28rem]'>
+                <div>
                     <Heading textColor='text-blush' fontSize='text-3xl'>
                         Share on social media
                     </Heading>
