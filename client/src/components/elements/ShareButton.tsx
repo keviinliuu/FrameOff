ca0536d5c71d0756ba559e44f271c2ab271a23f0
Kevin Liu <kevinliu676@yahoo.ca>
2024-02-28 21:36:46 -0500

    make share page responsive

--- client/src/components/elements/ShareButton.tsx
+++ client/src/components/elements/ShareButton.tsx
@@ -15,7 +15,7 @@ interface ShareButtonProps {
 export default function ShareButton({ children, bgColor, icon, onClick }: ShareButtonProps) {
     return (
         <button
-            className={`inline-flex items-center justify-center rounded ${bgColor} h-12 px-3 py-0.5 pt-2 font-main text-2xl text-white hover:scale-105 transition ease-in-out`}
+            className={`inline-flex items-center justify-center rounded ${bgColor} h-12 px-3 py-0.5 pt-2 font-main text-xl md:text-2xl text-white hover:scale-105 transition ease-in-out`}
             onClick={onClick}>
             <FontAwesomeIcon
                 icon={['fab', icon]}
--- client/src/pages/SharePoll.tsx
+++ client/src/pages/SharePoll.tsx
@@ -4,8 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import ShareButton from '../components/elements/ShareButton';
-import Heading from '../components/elements/Heading';
-import Logo from '../components/elements/Logo';
+import FrameOffLogo from '../assets/frameoff-logo.svg';
 import '../index.css';
 
 export default function SharePoll() {
@@ -36,30 +35,32 @@ export default function SharePoll() {
     }, [storePollId, storePollTitle, navigate]);
 
     return (
-        <div className='flex flex-col justify-center items-center min-h-screen w-screen'>
-            <div className='flex'>
-                <Logo />
+        <div className='flex flex-col justify-center items-center min-h-screen w-full bg-midnight'>
+            <div>
+                <div className='p-8 absolute top-0 left-0'>
+                    <a href='https://www.frameoff.com'>
+                        <img className='h-7 md:h-10 aspect-{25/6}' src={FrameOffLogo} />
+                    </a>
+                </div>
             </div>
 
-            <div className='flex flex-col items-center pb-24 px-4 pt-4'>
-                <Heading textColor='text-moonbeam' fontSize='text-3xl' textAlign='text-center'>
+            <div className='flex flex-col items-center pb-20 pt-2 md:pb-24 md:pt-4 px-4'>
+                <div className='text-moonbeam text-2xl pt-20 md:text-3xl md:pt-0 font-main text-center'>
                     Thanks for using FrameOff! Now go share your poll with the world 🌎
-                </Heading>
+                </div>
             </div>
 
-            <div className='flex flex-col pb-10 w-[28rem]'>
+            <div className='flex flex-col pb-5 md:pb-10 w-screen px-5 md:w-[28rem] md:px-0'>
                 <div className=''>
-                    <Heading textColor='text-blush' fontSize='text-3xl'>
-                        Share via link
-                    </Heading>
+                    <div className='text-2xl md:text-3xl text-blush'>Share via link</div>
                 </div>
 
                 <div className='flex h-12 flex-row rounded border-2 border-blush'>
-                    <Heading textColor='text-candy' fontSize='text-2xl' padding='pt-2 pl-4'>
+                    <div className='text-candy text-xl md:text-2xl pt-2 pl-4'>
                         {`${pollUrl}poll/${pollId}`}
-                    </Heading>
+                    </div>
 
-                    <div className='ml-auto  text-blush pt-0.5 text-2xl'>
+                    <div className='ml-auto text-blush pt-0.5 text-2xl'>
                         <SlCopyButton
                             value={`${pollUrl}poll/${pollId}`}
                             copy-label='Click to copy'
@@ -70,11 +71,11 @@ export default function SharePoll() {
                 </div>
             </div>
 
-            <div className='flex flex-col space-y-1.5 pt-20 w-[28rem]'>
+            <div className='flex flex-col space-y-1.5 pt-20 w-screen px-5 md:w-[28rem] md:px-0 pb-5'>
                 <div>
-                    <Heading textColor='text-blush' fontSize='text-3xl'>
+                    <div className='text-blush text-2xl md:text-3xl'>
                         Share on social media
-                    </Heading>
+                    </div>
                 </div>
 
                 <ShareButton
