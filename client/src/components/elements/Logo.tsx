6ece0059f8559dbc80bb12576341438a4e227383
DongtaiS <dongtaishi.w@gmail.com>
2024-02-29 21:26:26 -0500

    fix header logo link

--- client/src/components/elements/Logo.tsx
+++ client/src/components/elements/Logo.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import FrameOffLogo from '../../assets/frameoff-logo.svg';
 
 export default function Logo() {
     return (
         <div className='p-8 absolute top-0 left-0'>
-            <a href='https://www.frameoff.com'>
+            <Link to='..'>
                 <img className='h-10 aspect-{25/6}' src={FrameOffLogo} />
-            </a>
+            </Link>
         </div>
     );
 }
